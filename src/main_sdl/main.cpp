6d79de9d292ae42ee7175dbb1af7625cf236d1df
nightmareci <nightmareci@gmail.com>
2021-08-25 08:50:11 -0700

    Add all supported SDL_mixer audio file formats

--- src/main_sdl/main.cpp
+++ src/main_sdl/main.cpp
@@ -1,19 +1,26 @@
 #include "include.h"
 #include "gamestart.h"
 
-bool PollEvent(); // ƒCƒxƒ“ƒgˆ—
+bool PollEvent(); // ï¿½Cï¿½xï¿½ï¿½ï¿½gï¿½ï¿½ï¿½ï¿½
 
 int main(int argc, char* argv[])
 {
-	/* SDL‚Ì‰Šú‰» */
+	/* SDLï¿½Ìï¿½ï¿½ï¿½ï¿½ï¿½ */
 	if ( SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0 )
 	{
 		fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
 		return 1;
 	}
 
-	/* ƒTƒEƒ“ƒh‚Ì‰Šú‰» */
-	Mix_Init(MIX_INIT_MID);
+	/* ï¿½Tï¿½Eï¿½ï¿½ï¿½hï¿½Ìï¿½ï¿½ï¿½ï¿½ï¿½ */
+	Mix_Init(
+		MIX_INIT_FLAC |
+		MIX_INIT_MOD |
+		MIX_INIT_MP3 |
+		MIX_INIT_OGG |
+		MIX_INIT_MID |
+		MIX_INIT_OPUS
+	);
 	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 800);
 	Mix_AllocateChannels(100);
 

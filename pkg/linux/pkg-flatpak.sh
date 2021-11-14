75f70f368e405416da762e5b345b345e96c2a80f
nightmareci <nightmareci@gmail.com>
2021-11-14 11:39:14 -0800

    Change Linux package scripts to clean up build files

--- pkg/linux/pkg-flatpak.sh
+++ pkg/linux/pkg-flatpak.sh
@@ -16,3 +16,5 @@ cd "$BUILD_DIRECTORY" || exit 1
 
 flatpak-builder --repo=repo app "$SOURCE_DIRECTORY/pkg/linux/pkg-flatpak.yml" || exit 1
 flatpak build-bundle repo "$NAME-Linux.flatpak" "io.github.nightmareci.$NAME" || exit 1
+
+rm -rf app repo || exit 1

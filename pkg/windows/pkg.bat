c3428932ad3f23656e3aff7b6ffd45a866c350fc
Brandon McGriff <nightmareci@gmail.com>
2021-11-14 10:16:40 -0800

    Add error checking to Windows packaging script

--- pkg/windows/pkg.bat
+++ pkg/windows/pkg.bat
@@ -1,6 +1,6 @@
-@if "%~1"=="" goto error
-@if "%~2"=="" goto error
-@if "%~3"=="" goto error
+@if "%~1"=="" goto usage
+@if "%~2"=="" goto usage
+@if "%~3"=="" goto usage
 
 @set NAME=HeborisC7EX-SDL2
 @set VCPKG_TOOLCHAIN="%~1"
@@ -8,19 +8,33 @@
 @set BUILD_DIRECTORY="%~3"
 
 @rmdir /s /q "%BUILD_DIRECTORY%"
+@if %ERRORLEVEL% NEQ 0 goto error
 
 @cmake "%SOURCE_DIRECTORY%" -B "%BUILD_DIRECTORY%\build" -DPACKAGE_TYPE=Portable --toolchain "%VCPKG_TOOLCHAIN%" -DCMAKE_INSTALL_PREFIX="%BUILD_DIRECTORY%\%NAME%" -DVCPKG_TARGET_TRIPLET=x64-windows
+@if %ERRORLEVEL% NEQ 0 goto error
 @cmake --build "%BUILD_DIRECTORY%\build" --config Release
+@if %ERRORLEVEL% NEQ 0 goto error
 @cmake --install "%BUILD_DIRECTORY%\build"
+@if %ERRORLEVEL% NEQ 0 goto error
 
 @cd "%BUILD_DIRECTORY%"
+@if %ERRORLEVEL% NEQ 0 goto error
 @tar -c -a -f "%NAME%"-Windows.zip "%NAME%"
+@if %ERRORLEVEL% NEQ 0 goto error
 @cd ..
+@if %ERRORLEVEL% NEQ 0 goto error
 
 @rmdir /s /q "%BUILD_DIRECTORY%\build"
+@if %ERRORLEVEL% NEQ 0 goto error
 @rmdir /s /q "%BUILD_DIRECTORY%\%NAME%"
+@if %ERRORLEVEL% NEQ 0 goto error
 
 @exit
 
-:error
+:usage
 @echo Usage: pkg.bat [path-to-vcpkg.cmake] [source-directory] [build-directory]
+@exit
+
+:error
+echo Error encountered
+@exit

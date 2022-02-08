65b2bde7f1511ab97a03e04f1c20a7b3dc9eb285
zaphod77 <zaphod@tdl.com>
2022-02-08 10:05:56 -0600

    force CCW Rotation in sega old style.

--- src/script/ars.c
+++ src/script/ars.c
@@ -76,6 +76,10 @@ void statAMove(int32_t player, int32_t kickm, int32_t kickr) {
 			move = 0;
 		else
 			move = (getPushState(player, 5) || rolling) - (getPushState(player, 4) || getPushState(player, 6));
+		// for old style, force negative
+		if (repversw > 65 && (heboGB[player] == 2)) // sega rotation
+			move = abs(move)*-1;
+
 		if((move) && (isrotatelock[player] == 0)) {
 			bak = (rt[player] + move);
 				if(istrance[player]){

c96a74ed9b546b9c44478916416c8d283038922d
nightmareci <nightmareci@gmail.com>
2022-04-10 14:42:59 -0700

    Fix building for PlayStation Vita with latest Vita SDK packages

--- cmake/vita/Configure.cmake
+++ cmake/vita/Configure.cmake
@@ -1,3 +1,7 @@
+if(NOT DEFINED VITASDK)
+	message(FATAL_ERROR "The Vita SDK toolchain file must be provided when running CMake")
+endif()
+
 include("${VITASDK}/share/vita.cmake" REQUIRED)
 
 set(VITA_APP_NAME ${EXE})
@@ -8,21 +12,38 @@ set(VITA_VERSION  "00.01")
 
 set(VITA_MKSFOEX_FLAGS "${VITA_MKSFOEX_FLAGS} -d PARENTAL_LEVEL=1 -d ATTRIBUTE2=12")
 
-# Add all the files needed to compile here
 add_executable(${EXE} ${EXE_SOURCES})
 
 add_compile_definitions(USE_PHYSFS_SETSANECONFIG)
 
 target_link_directories(${EXE} PRIVATE ${BIN})
 
-# Libraries to link to (drop the -l prefix). This will mostly be stubs.
 target_link_libraries(${EXE}
-	SDL2
 	SDL2_image
 	SDL2_mixer
+	SDL2
+
+	physfs
+
+	jpeg
+	png
+	webp
+
+	mikmod
+	mpg123
+	vorbisfile
+	vorbis
+	ogg
+	FLAC
+
+	pthread
+	z
+	m
+
 	SceDisplay_stub
 	SceCtrl_stub
 	SceAudio_stub
+	SceAudioIn_stub
 	SceSysmodule_stub
 	SceGxm_stub
 	SceCommonDialog_stub
@@ -30,51 +51,36 @@ target_link_libraries(${EXE}
 	SceTouch_stub
 	SceHid_stub
 	SceMotion_stub
-	m
-	z
-	physfs
-	jpeg
-	png
-	webp
-	mikmod
-	mpg123
-	vorbis
-	vorbisfile
-	ogg
-	pthread
-	FLAC
 )
 
 target_include_directories(${EXE} PRIVATE
 	"${VITASDK}/arm-vita-eabi/include/SDL2"
 )
 
-# TODO: Rewrite to a list of all files instead.
-file( GLOB_RECURSE RES RELATIVE ${SRC} res/*)
-file( GLOB_RECURSE CONFIG RELATIVE ${SRC} config/*)
+file(GLOB_RECURSE RES RELATIVE ${SRC} res/*)
+file(GLOB_RECURSE CONFIG RELATIVE ${SRC} config/*)
 
 set(RESDATA "")
 
 foreach(ITEM ${RES})
-  list(APPEND RESDATA FILE)
-  list(APPEND RESDATA ${ITEM})
-  list(APPEND RESDATA ${ITEM})
+	list(APPEND RESDATA FILE)
+	list(APPEND RESDATA ${ITEM})
+	list(APPEND RESDATA ${ITEM})
 endforeach()
 
 set(CONFIGDATA "")
 
 foreach(ITEM ${CONFIG})
-  list(APPEND CONFIGDATA FILE)
-  list(APPEND CONFIGDATA ${ITEM})
-  list(APPEND CONFIGDATA ${ITEM})
+	list(APPEND CONFIGDATA FILE)
+	list(APPEND CONFIGDATA ${ITEM})
+	list(APPEND CONFIGDATA ${ITEM})
 endforeach()
 
-# Create Vita files
 vita_create_self(${EXE}.self ${EXE} UNSAFE)
 vita_create_vpk(${EXE}.vpk ${VITA_TITLEID} ${EXE}.self
-  VERSION ${VITA_VERSION}
-  NAME ${VITA_APP_NAME}
+	VERSION ${VITA_VERSION}
+	NAME ${VITA_APP_NAME}
 
-  ${CONFIGDATA}
-  ${RESDATA}
+	${CONFIGDATA}
+	${RESDATA}
 )

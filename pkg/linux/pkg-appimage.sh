d8a4311bb14480c97873dbc70d29485a81f9004a
nightmareci <nightmareci@gmail.com>
2023-12-01 13:03:14 -0800

    Add option to specify CMake generator in the AppImage packaging script

--- pkg/linux/pkg-appimage.sh
+++ pkg/linux/pkg-appimage.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
 if [ -z "$1" -o -z "$2" ] ; then
-	echo 'Usage: pkg-appimage.sh [source-directory] [build-directory]'
+	echo 'Usage: pkg-appimage.sh [source-directory] [build-directory] (cmake-generator)'
 	exit 1
 fi
 
 SOURCE_DIRECTORY=`readlink -f "$1"`
 BUILD_DIRECTORY=`readlink -f "$2"`
 
+if [ -z "$3" ] ; then
+	GENERATOR='Unix Makefiles'
+else
+	GENERATOR="$3"
+fi
+
 NAME="HeborisC7EX-SDL2"
 
 mkdir "$BUILD_DIRECTORY"
 cd "$BUILD_DIRECTORY" || exit 1
 
-cmake "$SOURCE_DIRECTORY" -G Ninja -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DPACKAGE_TYPE=Installable -DUSE_VENDOR_LIBRARIES=1 || exit 1
-cmake --build build || exit 1
+cmake "$SOURCE_DIRECTORY" -G "$GENERATOR" -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DPACKAGE_TYPE=Installable -DUSE_VENDOR_LIBRARIES=1 || exit 1
+cmake --build build -j `nproc` || exit 1
 DESTDIR=AppDir cmake --install build || exit 1
 
 LINUXDEPLOY="linuxdeploy-$(uname -m).AppImage"

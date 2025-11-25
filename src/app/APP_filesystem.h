f4676a79981985c1f198c1d76810439fb5978718
nightmareci <nightmareci@gmail.com>
2025-11-24 17:18:10 -0800

    Change plans for new file functions

--- src/app/APP_filesystem.h
+++ src/app/APP_filesystem.h
@@ -14,10 +14,9 @@ SDL_IOStream* APP_OpenAppend(const char* filename);
 bool APP_FileExists(const char* filename);
 
 // TODO: Refactor these file functions, e.g.:
-// bool APP_ReadFile32(const char* filename, int32_t* values, size_t start, size_t count);
-// bool APP_WriteFile32(const char* filename, int32_t* values, size_t start, size_t count);
-// bool APP_AppendFile32(const char* filename, int32_t* values, size_t count);
-// They shouldn't exit when errors occur, instead returning false
+// void APP_ReadFile32(const char* filename, int32_t* values, size_t start, size_t count);
+// void APP_WriteFile32(const char* filename, int32_t* values, size_t start, size_t count);
+// void APP_AppendFile32(const char* filename, int32_t* values, size_t count);
 
 void APP_LoadFile(const char* filename, void* buf, size_t size);
 void APP_ReadFile(const char* filename, void* buf, size_t size, size_t offset);
